c7b4ffa4dc8c20aec5ef187c5dae7e7b70cfe10f
Nicolas Davenne <nico.dvne@gmail.com>
2024-11-03 23:19:32 +0100

    move todo

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,29 @@
+'use client'
+
 import Todo from "@/components/Todo";
+import { Button } from "@/components/ui/button"
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const goToNew = () => {
+    router.push('/new');
+  }
+
   return (
     <div>
+      <h1 className='text-4xl font-extrabold uppercase'>
+        My personnal Todo App
+      </h1>
+
+      <div className='flex my-10 justify-end items-center up'>
+        <Button className='uppercase font-extrabold' onClick={goToNew}>
+          Add Todo
+        </Button>
+      </div>
+
       <Todo/>  
     </div>
   );
--- app/src/components/Todo.tsx
+++ app/src/components/Todo.tsx
@@ -1,29 +1,10 @@
-'use client'
-
 import React from 'react'
-import { Button } from "@/components/ui/button"
-import { useRouter } from 'next/navigation'
-
 
 const Todo = () => {
 
-  const router = useRouter();
-
-  const goToNew = () => {
-    router.push('/new');
-  }
-
   return (
     <div>
-      <h1 className='text-4xl font-extrabold uppercase'>
-        My personnal Todo App
-      </h1>
-
-      <div className='flex my-10 justify-end items-center up'>
-        <Button className='uppercase font-extrabold' onClick={goToNew}>
-          Add Todo
-        </Button>
-      </div>
+      
     </div>
   )
 }
